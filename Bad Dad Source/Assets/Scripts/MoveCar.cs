2149fcaee401e6c8df4cdf1b2de1ca40ad447660
Daniel <danielwinger1@gmail.com>
2020-12-14 12:20:59 -0500

    Updated To Current Version [Initial]
    I imported all the assets from what the game's current state is at. This should be considered the inital commit.

---
+++ Bad Dad Source/Assets/Scripts/MoveCar.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCar : MonoBehaviour
+{
+    [SerializeField] float driveSpeed = 1f;
+    Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        DriveCar();
+    }
+
+    private void DriveCar()
+    {
+        // Move up.
+        if (Input.GetAxis("Vertical") > 0)
+        {
+            rb.velocity = new Vector2(0, driveSpeed);
+        }
+        // Move down.
+        else if (Input.GetAxis("Vertical") < 0)
+        {
+            rb.velocity = new Vector2(0, -driveSpeed);
+        }
+
+        // Move right.
+        else if (Input.GetAxis("Horizontal") > 0)
+        {
+            rb.velocity = new Vector2(driveSpeed, 0);
+        }
+        // Move left.
+        else if (Input.GetAxis("Horizontal") < 0)
+        {
+            rb.velocity = new Vector2(-driveSpeed, 0);
+        }
+
+        else
+        {
+            rb.velocity = new Vector2(0, 0);
+        }
+    }
+}
