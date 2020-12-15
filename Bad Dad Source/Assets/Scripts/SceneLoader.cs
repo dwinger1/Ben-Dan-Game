70430584b7d31c4e2e163dc06cb3d6b6894885b9
Daniel <danielwinger1@gmail.com>
2020-12-15 13:18:48 -0500

    Added Scene Loading
    + Added a SceneLoader Script.
    + Added SceneLoader gameobject.
    + Added button functionality to the main menu play button.
    * Renamed "Test" scene to "Game."

---
+++ Bad Dad Source/Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    /// <summary>
+    /// This script will be used to control changing scenes throughout the game. 
+    /// The current iteration uses scene indexes to reference scenes.
+    /// </summary>
+
+    [SerializeField]
+    int gameSceneIndex = 1; // Using this as a way to change what scene index is considered to be for the "Game" scene from the editor.
+
+    // Load up the game scene.
+    public void LoadGameScene()
+    {
+        SceneManager.LoadScene(gameSceneIndex);
+    }
+}
