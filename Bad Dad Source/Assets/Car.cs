9a084fcbf6b39f1fcadc3b9809c17017d6c42c85
Daniel <danielwinger1@gmail.com>
2020-12-31 12:44:33 -0500

    New car spawner script
    Created a new car spawning script.

---
+++ Bad Dad Source/Assets/Car.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Car : MonoBehaviour
+{
+    /// <summary>
+    /// This script will hold car attributes for cars spawning in the left lane.
+    /// </summary>
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Bad Dad Source/Assets/Scripts/CarSpawner.cs
+++ Bad Dad Source/Assets/Scripts/CarSpawner.cs
@@ -6,15 +6,43 @@ using UnityEngine;
 /// </summary>
 public class CarSpawner : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    bool spawn = true;
+
+    [Header("Minimum and maximum time between new cars")]
+    [SerializeField] float spawnMinTime = 5f, spawnMaxTime = 10f;
+
+    [SerializeField] Car[] carPrefab; // Allow editor to drag and drop any number of car prefabs onto this spawner.
+
+    IEnumerator Start()
     {
-        
+        while (spawn)
+        {
+            // Wait for a random time between min and max to spawn a new car.
+            yield return new WaitForSeconds(Random.Range(spawnMinTime, spawnMaxTime));
+            Spawn();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private Car SpawnCar()
     {
-        
+        // Pick a random car from the list of prefabs added to this spawner.
+        int carIndex = Random.Range(0, carPrefab.Length);
+        return carPrefab[carIndex];
+    }
+
+    private void Spawn()
+    {
+        // Instantiate a new car gameobject prefab picked from SpawnCar method.
+        Car newCar = Instantiate(SpawnCar(), transform.position, transform.rotation) as Car;
+
+        // Nest the newly spawned car under the Car Spawner gameobject in the hierarchy.
+        newCar.transform.parent = transform;
+    }
+
+    public void StopSpawning()
+    {
+        // Add a way to shut off this spawner from another script.
+        spawn = false;
+        Debug.Log("Stop spawning cars!");
     }
 }
