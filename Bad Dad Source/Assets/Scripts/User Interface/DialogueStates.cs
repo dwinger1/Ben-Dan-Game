320e15d51d8cc902914fc28f8a54edcf3237dee8
Daniel <danielwinger1@gmail.com>
2021-01-12 21:35:59 -0500

    started added more functionality to dialogues
    Added a todo list
    Started pausegame method

--- Bad Dad Source/Assets/Scripts/User Interface/DialogueStates.cs
+++ Bad Dad Source/Assets/Scripts/User Interface/DialogueStates.cs
@@ -5,8 +5,14 @@ using UnityEngine;
 [CreateAssetMenu (menuName = "Dialogue State")]
 public class DialogueStates : ScriptableObject
 {
+    //TODO create bool for pausing game in the background when dialogue prompts
+    //TODO create a bool for game over trigger
+    //TODO create methods to enable manipulation of player stats through dialogue options (e.g. player earns money from interaction)
+
+
     [TextArea(10, 14)] [SerializeField] string dialogueText; // The editor's textbox input in the inspector.
     [SerializeField] DialogueStates[] nextState; // State machine states list
+    [SerializeField] bool willPauseGame = true;
     
     // Enable access to this object's text.
     public string GetText()
@@ -18,5 +24,15 @@ public class DialogueStates : ScriptableObject
     public DialogueStates[] GetNextState()
     {
         return nextState;
+    }
+
+    private void Pause()
+    {
+        if (willPauseGame)
+        {
+
+            /*https://gamedevbeginner.com/the-right-way-to-pause-the-game-in-unity/
+             * */
+        }
     }
 }
